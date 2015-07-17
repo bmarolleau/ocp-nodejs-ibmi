5fa2cfd0c1442f652f353cb33a3bb13de32af100
Christian Jorgensen <cj@dk.nmp.eu>
2015-07-17 18:03:51 +0200

    Add new dashboard: File waste information
    This dashboard shows the space occupied by deleted rows in tables in a schema.
    The view is ordered by the deleted data space in descending order.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 var db = require('/QOpenSys/QIBM/ProdData/Node/os400/db2i/lib/db2')
 
+app.locals._      = require('underscore');
+app.locals._.str  = require('underscore.string');
+
 db.debug(true)
 db.init()
 db.conn("*LOCAL")
@@ -31,24 +34,25 @@ app.get('/user/:id', function (req, res) {
 })
 app.get('/file_waste_schemas', function (req, res) {
   var sql = 
-    "SELECT SCHEMA_NAME, SCHEMA_OWNER, SCHEMA_TEXT FROM QSYS2.SYSSCHEMAS ORDER BY 1" 
+    "SELECT SYSTEM_SCHEMA_NAME, SCHEMA_OWNER, SCHEMA_TEXT FROM QSYS2.SYSSCHEMAS ORDER BY 1" 
   db.exec(sql, function(results) {
     res.render('file_waste_schemas', { title: 'File waste: Select schema', results: results})
   })
 })
 app.get('/file_waste/:id', function (req, res) {
   var sql = 
-    "select a.system_table_name, a.system_table_schema, b.system_table_member, " +
+    "select a.system_table_name, b.system_table_member, " +
 	"       number_rows, number_deleted_rows, " +
 	"       bigint( 100 * number_deleted_rows / max( number_rows+number_deleted_rows, 1 ) ) as Percent_Deleted, " +
 	"       data_size, " +
 	"       bigint( data_size * float( number_deleted_rows ) / max( number_rows+number_deleted_rows, 1 ) ) as Deleted_Space " +
 	"  from qsys2.systables a join qsys2.syspartitionstat b on (a.system_table_name, a.system_table_schema) = (b.system_table_name, b.system_table_schema) " +
 	" where a.table_schema = '" + req.params.id + "' " +
-	"       and table_type in ('T', 'P')and table_type in ('T', 'P') and file_type = 'D' and number_deleted_rows > 10000 " +
-	" order by 8 desc" 
+	"       and table_type in ('T', 'P')and table_type in ('T', 'P') and file_type = 'D' and number_deleted_rows > 0 " +
+	" order by 7 desc" +
+	" fetch first 100 rows only"
   db.exec(sql, function(results) {
-    res.render('file_waste', { title: 'File waste space information', results: results})
+    res.render('file_waste', { title: 'File waste space information - library ' + req.params.id, results: results})
   })
 })
 
