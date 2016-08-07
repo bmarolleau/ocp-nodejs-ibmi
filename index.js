74162871dac1bd3799380c744a9bcb61ae32ad56
Amad Gul <agul731@gmail.com>
2016-08-07 20:29:35 +0000

    Added DB2 Services and itoolkit examples

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var http = require('http')
 var io = require('socket.io')()
 var os = require('os')
 var fs = require('fs')
+var xt = require('/QOpenSys/QIBM/ProdData/Node/os400/xstoolkit/lib/itoolkit.js')
+var conn = new xt.iConn("*LOCAL");
+
 if (process.version.substring(0,3) == "v4.")
 	var db = require('/QOpenSys/QIBM/ProdData/OPS/Node4/os400/db2i/lib/db2');
 else
@@ -172,6 +175,312 @@ app.get('/ptf_group_info', function (req, res) {
     res.render('ptf_group_info', { title: title, results: results })
    })
  })
+ 
+ 
+app.get('/sysdiskstat', function(req, res) {
+    try {
+        db.exec("SELECT PERCENT_USED FROM QSYS2.SYSDISKSTAT", function(results) {
+        res.render('sysdiskstat', { title: 'SYSDISKSTAT', diskResults: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/SYSTEM_STATUS_INFO', function(req, res) {
+    var title = "SYSTEM_STATUS_INFO"
+    var sql = "SELECT * FROM QSYS2.SYSTEM_STATUS_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.SYSTEM_STATUS_INFO"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+
+app.get('/LICENSE_INFO', function(req, res) {
+    var title = "LICENSE_INFO"
+    var sql = "SELECT * FROM QSYS2.LICENSE_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.LICENSE_INFO"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/JOURNAL_INFO', function(req, res) {
+    var title = "JOURNAL_INFO"
+    var sql = "SELECT * FROM QSYS2.JOURNAL_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.JOURNAL_INFO%20view"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/USER_INFO', function(req, res) {
+    var title = "USER_INFO"
+    var sql = "SELECT * FROM QSYS2.USER_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.USER_INFO%20catalog"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/TCPIP_INFO', function(req, res) {
+    var title = "TCPIP_INFO"
+    var sql = "SELECT * FROM QSYS2.TCPIP_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.TCPIP_INFO%20view"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/NETSTAT_INFO', function(req, res) {
+    var title = "NETSTAT_INFO"
+    var sql = "SELECT * FROM QSYS2.NETSTAT_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.NETSTAT_INFO"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/USER_STORAGE', function(req, res) {
+    var title = "USER_STORAGE"
+    var sql = "SELECT * FROM QSYS2/USER_STORAGE"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.USER_STORAGE%20catalog"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/OUTPUT_QUEUE_INFO', function(req, res) {
+    var title = "OUTPUT_QUEUE_INFO"
+    var sql = "SELECT * FROM QSYS2.OUTPUT_QUEUE_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.OUTPUT_QUEUE_INFO%20-%20View"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/SERVER_SBS_ROUTING', function(req, res) {
+    var title = "SERVER_SBS_ROUTING"
+    var sql = "SELECT * FROM QSYS2.SERVER_SBS_ROUTING"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.SERVER_SBS_ROUTING%20-%20view"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/SYSTEM_VALUE_INFO', function(req, res) {
+    var title = "SYSTEM_VALUE_INFO"
+    var sql = "SELECT * FROM SYSTEM_VALUE_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.SYSTEM_VALUE_INFO%20catalog"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/ACTIVE_JOB_INFO', function(req, res) {
+    var title = "ACTIVE_JOB_INFO"
+    var sql = "SELECT 8 FROM QSYS2.ACTIVE_JOB_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.ACTIVE_JOB_INFO()%20-%20UDTF"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/GROUP_PROFILE_ENTRIES', function(req, res) {
+    var title = "GROUP_PROFILE_ENTRIES"
+    var sql = "SELECT * FROM QSYS2.GROUP_PROFILE_ENTRIES"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.GROUP_PROFILE_ENTRIES%20%E2%80%93%20new%20security%20view"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/DRDA_AUTHENTICATION_ENTRY_INFO', function(req, res) {
+    var title = "DRDA_AUTHENTICATION_ENTRY_INFO"
+    var sql = "SELECT * FROM QSYS2.DRDA_AUTHENTICATION_ENTRY_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.DRDA_AUTHENTICATION_ENTRY_INFO"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/ENVIRONMENT_VARIABLE_INFO', function(req, res) {
+    var title = "ENVIRONMENT_VARIABLE_INFO"
+    var sql = "SELECT * FROM QSYS2.ENVIRONMENT_VARIABLE_INFO"
+    var url = "https://www.ibm.com/developerworks/community/wikis/home?lang=en#!/wiki/IBM%20i%20Technology%20Updates/page/QSYS2.ENVIRONMENT_VARIABLE_INFO%20-%20view"
+    try {
+        db.exec(sql, function(results) {
+        res.render('table_view', { title: title, sql: sql, url: url, results: results});
+        //console.log(results);
+        }); 
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+// IBM i program/service call
+app.get('/IBMi_program_call', function(req, res) {
+    db.close()
+    var title = "IBM i program/service call"
+    var cmd = "QTOCNETSTS"
+    var sysVal = "QCCSID";
+        var errno = [
+            [0, "10i0"],
+            [0, "10i0", {"setlen":"rec2"}],
+            ["", "7A"],
+            ["", "1A"]
+        ];
+        var outBuf = [
+            [0, "10i0"],     // [0] Bytes returned
+            [0, "10i0"],     // [1] Bytes available
+            [0, "10i0"],     // [2] TCP/IPv4 stack status
+            [0, "10i0"],     // [3] How long active
+            ["", "8A"],     // [4] When last started - date
+            ["", "6A"],     // [5] When last started - time
+            ["", "8A"],     // [6] When last ended - date
+            ["", "6A"],     // [7] When last ended - time
+            ["", "10A"],    // [8] Who last started - job name
+            ["", "10A"],    // [9] Who last started - job user name
+            ["", "6A"],     // [10] Who last started - job number
+            ["", "16h"],    // [11] Who last started - internal job identifier
+            ["", "10A"],    // [12] Who last ended - job name
+            ["", "10A"],    // [13] Who last ended - job user name
+            ["", "6A"],     // [14] Who last ended - job number
+            ["", "16h"],    // [15] Who last ended - internal job identifier
+            [0, "10i0"],     // [16] Offset to additional information
+            [0, "10i0"],     // [17] Length of additional information
+            [0, "10i0"],     // [18] Limited mode
+            [0, "10i0"],     // [19] Offset to list of Internet addresses
+            [0, "10i0"],     // [20] Number of Internet addresses
+            [0, "10i0"],     // [21] Entry length for list of Internet addresses
+            [0, "10i0"],     // [22] DNS protocol
+            [0, "10i0"],     // [23] Retries
+            [0, "10i0"],     // [24] Time interval
+            [0, "10i0"],     // [25] Search order
+            [0, "10i0"],     // [26] Initial domain name server
+            [0, "10i0"],     // [27] DNS listening port
+            ["", "64A"],    // [28] Host name
+            ["", "255A"],  // [29] Domain name
+            ["", "1A"],     // [30] Reserved
+            ["", "256A"],  // [31] Domain search list
+        ];
+        var pgm = new xt.iPgm("QTOCNETSTS", {"lib":"QSYS","func":"QtocRtvTCPA"});
+        pgm.addParam(outBuf, {"io":"out", "len":"rec1"});
+        pgm.addParam(0, "10i0", {"setlen":"rec1"});
+        pgm.addParam("TCPA0300", "8A");
+        pgm.addParam(errno, {"io":"both", "len" : "rec2"});
+        conn.add(pgm);
+        conn.run(function(results) {
+            var pram1 = "Domain: "+xt.xmlToJson(results)[0].data[29].value
+            var pram2 = "Host: "+xt.xmlToJson(results)[0].data[28].value
+            res.render('node_toolkit', { title: title, cmd: cmd, pram1: pram1, pram2: pram2})
+            db.open()
+        });
+});
+
+// CL Command
+app.get('/IBMi_cl_command', function(req, res) {
+    db.close()
+    var title = "IBM i CL Command"
+    var cmd = "RTVJOBA USRLIBL(?) SYSLIBL(?)"
+    conn.add(xt.iCmd("RTVJOBA USRLIBL(?) SYSLIBL(?)"));
+    conn.run(function(results) {
+        var pram1 = "USRLIBL: " + xt.xmlToJson(results)[0].data[0].value 
+        var pram2 = "SYSLIBL: " + xt.xmlToJson(results)[0].data[1].value
+        res.render('node_toolkit', { title: title, cmd: cmd, pram1: pram1, pram2: pram2})
+        db.open()
+    });
+});
+
+function testQurey() {
+    // QShell Command (PASE) 
+    conn.add(xt.iSh("system -i wrksyssts"));
+    conn.run(function(results) {
+        console.log(results);
+    });
+}
 
 
 http.createServer(function(req, res) {
